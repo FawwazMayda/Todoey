036633e0c46b326a4bbf54d6390769960f1d9cc3
fawwazmayda <maydafawwaz@gmail.com>
2020-04-04 22:40:07 +0800

    UI TableView dataSource delegate

---
+++ ToDoEy/Const.swift
@@ -0,0 +1,11 @@
+//
+//  Const.swift
+//  ToDoEy
+//
+//  Created by Muhammad Fawwaz Mayda on 04/04/20.
+//  Copyright © 2020 Muhammad Fawwaz Mayda. All rights reserved.
+//
+
+struct Constant {
+    static let cellIdentifier = "ToDoItemCell"
+}
--- ToDoEy/TodoeyViewController.swift
+++ ToDoEy/TodoeyViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class TodoeyViewController: UITableViewController {
+    let arrayData = ["Abang","Bola","Kotak"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
-
+    
+    //MARK: TableView DataSoource
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        arrayData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constant.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = arrayData[indexPath.row]
+        return cell
+    }
 
 }
 
