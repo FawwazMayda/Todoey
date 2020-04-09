00c3c0dc4aa1e4d60b6b3b820c237bad93c48d0c
fawwazmayda <maydafawwaz@gmail.com>
2020-04-09 19:49:09 +0800

    can saving data

--- ToDoEy/TodoeyViewController.swift
+++ ToDoEy/TodoeyViewController.swift
@@ -8,12 +8,26 @@
 
 import UIKit
 
+class Item: Codable {
+    var item : String
+    var done : Bool
+    init(item : String,done : Bool) {
+        self.item = item
+        self.done = done
+    }
+}
 class TodoeyViewController: UITableViewController {
-    var arrayData = ["Abang","Bola","Kotak"]
+    var arrayData = [Item]()
+    
+    var dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(dataFilePath)
+        //arrayData.append(Item(item: "Abang", done: true))
+        //arrayData.append(Item(item: "Kotak", done: false))
+        self.loadData()
         tableView.dataSource = self
         tableView.delegate = self
     }
@@ -29,33 +43,29 @@ class TodoeyViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: Constant.cellIdentifier, for: indexPath)
-        cell.textLabel?.text = arrayData[indexPath.row]
+        cell.textLabel?.text = arrayData[indexPath.row].item
+        cell.accessoryType = arrayData[indexPath.row].done ? .checkmark : .none
         return cell
     }
     
     //MARK: TableView Delegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let check = UITableViewCell.AccessoryType.checkmark
-        let none = UITableViewCell.AccessoryType.none
-        if tableView.cellForRow(at: indexPath)?.accessoryType==check {
-            tableView.cellForRow(at: indexPath)?.accessoryType=none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType=check
-        }
-        
-        tableView.deselectRow(at: indexPath, animated: true)
+        arrayData[indexPath.row].done = !arrayData[indexPath.row].done
+        self.saveData()
+        tableView.reloadData()
     }
     
-    //MARK: IBAction
+    //MARK: IBAction and Alert
     
     @IBAction func addNewItem(_ sender: UIBarButtonItem) {
         
         var alerTextField = UITextField()
         let alert = UIAlertController(title: "Add a new Item", message: "OH", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (UIAlertAction) in
-            print(alerTextField.text)
-            self.arrayData.append(alerTextField.text!)
+            //print(alerTextField.text)
+            self.arrayData.append(Item(item: alerTextField.text!, done: false))
             self.tableView.reloadData()
+            self.saveData()
         }
         
         alert.addTextField { (UITextField) in
@@ -64,6 +74,25 @@ class TodoeyViewController: UITableViewController {
         alert.addAction(action)
         present(alert,animated: true)
         
+    }
+    
+    func saveData() {
+        do {
+            let data = try PropertyListEncoder().encode(self.arrayData)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error Encoding data")
+        }
+    }
+    
+    func loadData() {
+        do {
+            if let data = try? Data(contentsOf: dataFilePath!) {
+                self.arrayData = try PropertyListDecoder().decode([Item].self, from: data)
+            }
+        } catch {
+            print("Error Decoding data")
+        }
     }
     
 }
